41f7760b91909b3324677c02d02fa2e0a71b5318
zhanghuize <56755115+zhanghuize-git@users.noreply.github.com>
2023-06-13 14:56:49 +0800

    feat: add digital sign formatter in cell formatter

--- src/utils/common-utils.js
+++ src/utils/common-utils.js
@@ -53,6 +53,13 @@ export const getMediaUrl = () => {
   return window.dtablePluginConfig.mediaUrl;
 };
 
+export const getValueFromPluginConfig = (attribute) => {
+  if (window.dtable) {
+    return window.dtable[attribute];
+  }
+  return window.dtablePluginConfig[attribute];
+};
+
 export const isValidEmail = (email) => {
   const reg = /^[A-Za-zd]+([-_.][A-Za-zd]+)*@([A-Za-zd]+[-.])+[A-Za-zd]{2,6}$/;
   return reg.test(email);
